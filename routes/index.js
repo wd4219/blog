d7447d8e8920fceea6c18dd96aa35f351a124039
wangdi <wangdi2@myhexin.com>
2017-08-23 17:10:55 +0800

    20170823提交

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const router = require('koa-router')()
+const marked = require('marked');
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: '王迪的博客-前端笔记与交流'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+router.get('/article',async (ctx,next)=>{
+  await ctx.render('article',{
+    title:'文章标题',
+    article:marked("```javascript\n const a = 1;\nconst a = 1;\nconst a = 1;\n```")
+  });
+});
+module.exports = router
